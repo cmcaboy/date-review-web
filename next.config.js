0c0b2c6b8f323342b11e41890a9366bda52f7844
Cory McAboy <cory.mcaboy@gmail.com>
2019-02-04 08:39:58 -0500

    changed next.config.js to call withTypescript within module.exports

--- next.config.js
+++ next.config.js
@@ -1,15 +1,40 @@
 // next.config.js
-const withTypescript = require("@zeit/next-typescript");
+// const withTypescript = require("@zeit/next-typescript");
+
+// module.exports = withTypescript({
+//   webpack(config) {
+//     config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+
+//     return config;
+//   }
+// });
 const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack");
 
-module.exports = withTypescript({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+// const { PHASE_PRODUCTION_SERVER } = require("next-server/constants");
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === "development"
+    ? {}
+    : !process.env.NOW_REGION
+    ? require("next/constants")
+    : require("next-server/constants");
 
-    return config;
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {
+      /* production only config */
+    };
   }
-});
+  const withTypescript = require("@zeit/next-typescript");
+  return withTypescript({
+    webpack(config, options) {
+      // Do not run type checking twice:
+      if (options.isServer)
+        config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+      return config;
+    }
+  });
+};
 
 // module.exports = {
 //   webpack: config => {
