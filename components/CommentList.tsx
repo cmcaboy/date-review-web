7555ae0e5369d5e9ec4304c67d0e5da4a2b19808
Cory McAboy <cory.mcaboy@gmail.com>
2019-02-11 07:41:56 -0500

    enabled show comments / hide comments

--- components/CommentList.tsx
+++ components/CommentList.tsx
@@ -1,18 +1,33 @@
 import * as React from "react";
 import { FindCommentsComponent } from "../generated/apolloComponents";
 import CommentItem from "./CommentItem";
+import { P, HR } from "./common";
+import { LightText } from "./common/LightText";
+import styled from "styled-components";
+
+// TODO: Only fetch comments if requested by the user
 
 interface CommentListProps {
   id: string;
 }
 
-interface CommentListState {}
+interface CommentListState {
+  showComments: boolean;
+}
 
 class CommentList extends React.Component<CommentListProps, CommentListState> {
   constructor(props: CommentListProps) {
     super(props);
+
+    this.state = {
+      showComments: false
+    };
   }
-  public render(): JSX.Element {
+
+  ToggleComments = () =>
+    this.setState(prevState => ({ showComments: !prevState.showComments }));
+
+  render(): JSX.Element {
     return (
       <FindCommentsComponent variables={{ reviewId: this.props.id }}>
         {({ data, loading, error }) => {
@@ -28,13 +43,46 @@ class CommentList extends React.Component<CommentListProps, CommentListState> {
           if (data && !data.findComments) {
             return null;
           }
-          return data.findComments.map(comment => (
-            <CommentItem key={comment.id} comment={comment} />
-          ));
+          // * Hide comments by default. Display them if user requests
+          if (!this.state.showComments) {
+            return (
+              <CommentsText onClick={this.ToggleComments}>
+                Show comments ({data.findComments.length})
+              </CommentsText>
+            );
+          }
+          return (
+            <>
+              <CommentsText onClick={this.ToggleComments}>
+                Hide comments
+              </CommentsText>
+              <ThisHR />
+              {data.findComments.map(comment => (
+                <CommentItem key={comment.id} comment={comment} />
+              ))}
+            </>
+          );
         }}
       </FindCommentsComponent>
     );
   }
 }
+
+const CommentsText = styled(LightText)`
+  align-self: right;
+  text-align: right;
+  margin-top: 10px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ThisHR = styled(HR)`
+  opacity: 0.6;
+  color: gray;
+  margin-left: -5px;
+  margin-right: -5px;
+  width: auto;
+`;
 
 export { CommentList };
