440955ad94fdb06cca8cb45858294d7075909c94
Cory McAboy <cory.mcaboy@gmail.com>
2019-02-13 07:57:50 -0500

    setup nextroutes

--- server/routes.ts
+++ server/routes.ts
@@ -5,4 +5,8 @@ export const routes = nextRoutes() as Routes;
 export const Router = routes.Router;
 export const Link = routes.Link;
 
-routes.add("user", "/user/:id/");
+routes.add("user", "/user/:id");
+routes.add("index", "/");
+routes.add("new", "/new");
+routes.add("about", "/about");
+routes.add("contact", "/contact");
